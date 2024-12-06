dc422a139266a9c0c42a7483c7c1d0bf199aeae7
breakingbadhabits <70371792+breakingbadhabits@users.noreply.github.com>
2024-12-06 16:18:32 +0800

    Created new form for notifs

---
+++ AnotherSample/Notif.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnotherSample
+{
+    public partial class Notif : Form
+    {
+        public Notif()
+        {
+            InitializeComponent();
+        }
+
+        private void Notif_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
